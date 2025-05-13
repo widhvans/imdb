6d3e677872737661ca0fc784f8d1afa37ed075f7
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-13 18:55:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,33 +3,36 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters
 import aiohttp
 import config
 import logging
+import urllib.parse
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def start(update, context):
-    await update.message.reply_text('Hi! Send me a movie name, and I’ll fetch its poster and IMDb details.')
+    await update.message.reply_text('Hi! Send me a movie name, and I’ll fetch its poster and details.')
 
 async def search_movie(update, context):
-    movie_name = update.message.text
-    api_key = config.OMDB_API_KEY
-    url = f'http://www.omdbapi.com/?t={movie_name}&apikey={api_key}'
+    movie_name = urllib.parse.quote(update.message.text)
+    api_key = config.TMDB_API_KEY
+    url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_name}'
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.json()
 
-            if data['Response'] == 'True':
-                title = data['Title']
-                year = data['Year']
-                imdb_rating = data['imdbRating']
-                plot = data['Plot']
-                poster = data['Poster']
+            if data['results']:
+                movie = data['results'][0]
+                title = movie['title']
+                year = movie['release_date'][:4] if movie['release_date'] else 'N/A'
+                rating = movie['vote_average'] if movie['vote_average'] else 'N/A'
+                overview = movie['overview'] if movie['overview'] else 'No plot available'
+                poster_path = movie['poster_path']
 
-                message = f"**{title} ({year})**\nIMDb Rating: {imdb_rating}\nPlot: {plot}"
-                if poster != 'N/A':
-                    await update.message.reply_photo(photo=poster, caption=message, parse_mode='Markdown')
+                message = f"**{title} ({year})**\nTMDb Rating: {rating}\nPlot: {overview}"
+                if poster_path:
+                    poster_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
+                    await update.message.reply_photo(photo=poster_url, caption=message, parse_mode='Markdown')
                 else:
                     await update.message.reply_text(message, parse_mode='Markdown')
             else:
